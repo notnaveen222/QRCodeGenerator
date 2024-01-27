9389fc292ff7d6c32e157253df152452c9031146
Naveen <notnaveen2@gmail.com>
2024-01-27 09:48:03 +0530

    Created Extension For chrome

---
+++ Extension/script.js
@@ -0,0 +1,26 @@
+addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter'){
+        var input = document.getElementById('UserInput').value;
+        if (input != ''){
+            var response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${input}`);
+            var image = document.getElementById('QR');
+            image.classList.remove('image-blur')
+            if(response.ok) {
+                const blob = await response.blob();
+                const imageURL = URL.createObjectURL(blob);
+                document.getElementById('QR').src = imageURL;
+                document.getElementsByClassName('err')[0].style.opacity = '0%';
+            }
+    
+        } else {
+            var image = document.getElementById('QR');
+            image.classList.add('image-blur');
+            document.getElementsByClassName('err')[0].style.opacity = '100%';
+
+        }
+    }
+})
+
+document.getElementById('close-button').addEventListener('click',() => {
+    window.close();
+})
